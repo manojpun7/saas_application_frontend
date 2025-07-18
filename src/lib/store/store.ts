6383a580b6c0b0eec1726284cf3f9d0a56fd6946
Manoj <punmanoj2060@gmail.com>
2025-07-19 00:02:37 +0545

    configure project state

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,16 +1,18 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import studentSlice from "./studentSlice";
-// import userSlice from "./userSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from './auth/authSlice'
+import teacherSlice from './teacher/teacherSlice'
+import instituteSlice from './institute/instituteSlice'
 
-// const store = configureStore({
-//   reducer: {
-//     userSlice: userSlice,
-//     studentSlice: studentSlice,
-//   },
-// });
+const store = configureStore({
+  reducer: {
+   auth :authSlice,
+   teacher: teacherSlice,
+   institute: instituteSlice
+  },
+});
 
 
-// export default store
+export default store
 
-// export type AppDispatch = typeof store.dispatch //useDispatch lai type 
-// export type RootState = ReturnType<typeof store.getState>//useSelector type
+export type AppDispatch = typeof store.dispatch //useDispatch lai type 
+export type RootState = ReturnType<typeof store.getState>//useSelector type
