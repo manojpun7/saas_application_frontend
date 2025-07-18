69bf9d73058e8c17cd4d1a9cbfb2c5abc0bc29ac
Manoj <punmanoj2060@gmail.com>
2025-07-18 23:26:15 +0545

    login and register form

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,16 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import studentSlice from "./studentSlice";
-import userSlice from "./userSlice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import studentSlice from "./studentSlice";
+// import userSlice from "./userSlice";
 
-const store = configureStore({
-  reducer: {
-    userSlice: userSlice,
-    studentSlice: studentSlice,
-  },
-});
+// const store = configureStore({
+//   reducer: {
+//     userSlice: userSlice,
+//     studentSlice: studentSlice,
+//   },
+// });
 
 
-export default store
+// export default store
 
-export type AppDispatch = typeof store.dispatch //useDispatch lai type 
-export type RootState = ReturnType<typeof store.getState>//useSelector type
+// export type AppDispatch = typeof store.dispatch //useDispatch lai type 
+// export type RootState = ReturnType<typeof store.getState>//useSelector type
