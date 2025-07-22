f13e352ab4f97c97a075df6c23b5ceed219a7a9b
Manoj <punmanoj2060@gmail.com>
2025-07-22 10:15:18 +0545

    institute-course-slice

---
+++ src/lib/store/institute/course/institute-course-type.ts
@@ -0,0 +1,12 @@
+import { Status } from "@/lib/types/type";
+
+interface IInstituteCourseInitialDataCourse{
+    courseName : string, 
+    coursePrice : string, 
+    id : string
+}
+
+export interface IInstituteCourseInitialData{
+    status : Status, 
+    courses : IInstituteCourseInitialDataCourse[]
+}
