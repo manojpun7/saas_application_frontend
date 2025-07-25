d41a36239f7186593be304bbb6495ffdf7f76faf
Manoj <punmanoj2060@gmail.com>
2025-07-25 13:02:37 +0545

    category actions

--- src/lib/store/institute/category/categorySlice.ts
+++ src/lib/store/institute/category/categorySlice.ts
@@ -45,3 +45,37 @@ export function fetchCategories (){
         }
     }   
 }
+export function addCategories (data: ICategoryData){
+    return async function fetchCategoriesThunk(dispatch:AppDispatch) {
+        try{
+            const response = await APIWITHTOKEN.post("institute/category",data)
+            if(response.status=== 201){
+                dispatch(setStatus(Status.SUCCESS))
+                response.data.data.length > 0 && dispatch(setData(response.data.data))
+            }
+            else{
+                dispatch(setStatus(Status.ERROR))
+            }
+        }catch(error){
+            console.log(error)
+            dispatch(setStatus(Status.ERROR))
+        }
+    }   
+}
+
+
+export function deleteCategory(id:string){
+    return async function deleteCategoryThunk(dispatch:AppDispatch){
+        try {
+            const response = await APIWITHTOKEN.delete("institute/category/" + id)
+            if(response.status === 200){
+                dispatch(setStatus(Status.SUCCESS))
+            }else{
+                dispatch(setStatus(Status.ERROR))
+            }
+        } catch (error) {
+            console.log(error)
+            dispatch(setStatus(Status.ERROR))
+        }
+    }
+}
